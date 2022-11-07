f382e7b04b0d13bfc9bf29f986747fd168b33e5a
Ekaterina Abramovich <112434872+ekaterina392@users.noreply.github.com>
2022-11-07 18:46:50 -0500

    Trying to use Destroy(Object);

--- Final project/Assets/Scripts (scene 2)/IngredientsController.cs
+++ Final project/Assets/Scripts (scene 2)/IngredientsController.cs
@@ -4,11 +4,26 @@ using UnityEngine;
 
 public class IngredientsController : MonoBehaviour
 {
+    //Three ingredients
+    public GameObject Sphere;
+    public GameObject Cylinder;
+    public GameObject Cube;
+
+    
+    //Potion
+    //public GameObject Capsule;
+
     private void OnTriggerEnter(Collider collider)
     {
-        if (collider.gameObject.name == "Cube")
+        if (collider.gameObject.name == "Sphere")
         {
-            Debug.Log("Collision!");
+            Destroy(Sphere);
+        } else if (collider.gameObject.name == "Cylinder")
+        {
+            Destroy(Cylinder);
+        } else if (collider.gameObject.name == "Cube")
+        {
+            Destroy(Cube);
         }
     }
 }
