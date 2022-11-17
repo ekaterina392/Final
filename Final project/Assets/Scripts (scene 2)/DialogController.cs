da54daff288f3e66e2338efedfa8897807967ddc
Ekaterina Abramovich <112434872+ekaterina392@users.noreply.github.com>
2022-11-16 19:29:33 -0500

    Distance changed from 3 to 5

--- Final project/Assets/Scripts (scene 2)/DialogController.cs
+++ Final project/Assets/Scripts (scene 2)/DialogController.cs
@@ -51,90 +51,90 @@ public class DialogController : MonoBehaviour
     void Update ()
     {
         //Centaur
-        if (Vector3. Distance(gameObject.transform.position, Centaur.transform.position) < 3 && Input.GetKeyDown(KeyCode.T))
+        if (Vector3. Distance(gameObject.transform.position, Centaur.transform.position) < 5 && Input.GetKeyDown(KeyCode.T))
         {
             CanvasCentaur.SetActive(true);
         }
         
-        if (Vector3. Distance(gameObject.transform.position, Centaur.transform.position) > 3)
+        if (Vector3. Distance(gameObject.transform.position, Centaur.transform.position) > 5)
         {
             CanvasCentaur.SetActive(false);
         }
     
         //Crab
-        if (Vector3. Distance(gameObject.transform.position, Crab.transform.position) < 3 && Input.GetKeyDown(KeyCode.T))
+        if (Vector3. Distance(gameObject.transform.position, Crab.transform.position) < 5 && Input.GetKeyDown(KeyCode.T))
         {
             CanvasCrab.SetActive(true);
         }
         
-        if (Vector3. Distance(gameObject.transform.position, Crab.transform.position) > 3)
+        if (Vector3. Distance(gameObject.transform.position, Crab.transform.position) > 5)
         {
             CanvasCrab.SetActive(false);
         }
         
         //Titan
-        if (Vector3. Distance(gameObject.transform.position, Titan.transform.position) < 3 && Input.GetKeyDown(KeyCode.T))
+        if (Vector3. Distance(gameObject.transform.position, Titan.transform.position) < 5 && Input.GetKeyDown(KeyCode.T))
         {
             CanvasTitan.SetActive(true);
         }
         
-        if (Vector3. Distance(gameObject.transform.position, Titan.transform.position) > 3)
+        if (Vector3. Distance(gameObject.transform.position, Titan.transform.position) > 5)
         {
             CanvasTitan.SetActive(false);
         }
         
         //Cave man
-        if (Vector3. Distance(gameObject.transform.position, CaveMan.transform.position) < 3 && Input.GetKeyDown(KeyCode.T))
+        if (Vector3. Distance(gameObject.transform.position, CaveMan.transform.position) < 5 && Input.GetKeyDown(KeyCode.T))
         {
             CanvasCaveMan.SetActive(true);
         }
         
-        if (Vector3. Distance(gameObject.transform.position, CaveMan.transform.position) > 3)
+        if (Vector3. Distance(gameObject.transform.position, CaveMan.transform.position) > 5)
         {
             CanvasCaveMan.SetActive(false);
         }
         
         //Troll
-        if (Vector3. Distance(gameObject.transform.position, Troll.transform.position) < 3 && Input.GetKeyDown(KeyCode.T))
+        if (Vector3. Distance(gameObject.transform.position, Troll.transform.position) < 5 && Input.GetKeyDown(KeyCode.T))
         {
             CanvasTroll.SetActive(true);
         }
         
-        if (Vector3. Distance(gameObject.transform.position, Troll.transform.position) > 3)
+        if (Vector3. Distance(gameObject.transform.position, Troll.transform.position) > 5)
         {
             CanvasTroll.SetActive(false);
         }
         
         //Demon girl
-        if (Vector3. Distance(gameObject.transform.position, DemonGirl.transform.position) < 3 && Input.GetKeyDown(KeyCode.T))
+        if (Vector3. Distance(gameObject.transform.position, DemonGirl.transform.position) < 5 && Input.GetKeyDown(KeyCode.T))
         {
             CanvasDemonGirl.SetActive(true);
         }
             
-        if (Vector3. Distance(gameObject.transform.position, DemonGirl.transform.position) > 3)
+        if (Vector3. Distance(gameObject.transform.position, DemonGirl.transform.position) > 5)
         {
             CanvasDemonGirl.SetActive(false);
         }
         
         //Piggy orc
-        if (Vector3. Distance(gameObject.transform.position, PiggyOrc.transform.position) < 3 && Input.GetKeyDown(KeyCode.T))
+        if (Vector3. Distance(gameObject.transform.position, PiggyOrc.transform.position) < 5 && Input.GetKeyDown(KeyCode.T))
         {
             CanvasPiggyOrc.SetActive(true);
         }
             
-        if (Vector3. Distance(gameObject.transform.position, PiggyOrc.transform.position) > 3)
+        if (Vector3. Distance(gameObject.transform.position, PiggyOrc.transform.position) > 5)
         {
             CanvasPiggyOrc.SetActive(false);
         }
         
         
         //Woman warrior
-        if (Vector3. Distance(gameObject.transform.position, WomanWarrior.transform.position) < 3 && Input.GetKeyDown(KeyCode.T))
+        if (Vector3. Distance(gameObject.transform.position, WomanWarrior.transform.position) < 5 && Input.GetKeyDown(KeyCode.T))
         {
             CanvasWomanWarrior.SetActive(true);
         }
             
-        if (Vector3. Distance(gameObject.transform.position, WomanWarrior.transform.position) > 3)
+        if (Vector3. Distance(gameObject.transform.position, WomanWarrior.transform.position) > 5)
         {
             CanvasWomanWarrior.SetActive(false);
         }
