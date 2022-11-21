3aa99e5e201b1eda65eb950cc637153019f7c9b9
Ekaterina Abramovich <112434872+ekaterina392@users.noreply.github.com>
2022-11-21 15:12:01 -0500

    Cannon shooting added

---
+++ Final project/Assets/Scripts (scene 3)/CannonControl.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonControl : MonoBehaviour
+{
+    Transform _Player;
+    private float distance;
+    public float howClose;
+    public Transform head, barrel;
+
+    public GameObject _projectile;
+
+    public float fireRate, nextFire;
+
+    private void Start()
+    {
+        _Player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    private void Update()
+    {
+        distance = Vector3.Distance(_Player.position, transform.position);
+        if (distance <= howClose)
+        {
+            head.LookAt(_Player);
+            if (Time.time >= nextFire)
+            {
+                nextFire = Time.time + 1f / fireRate;
+                shoot();
+            }
+        }
+    }
+
+    void shoot()
+    {
+        GameObject clone = Instantiate(_projectile, barrel.position, head.rotation);
+        //force forward 1500    
+        clone.GetComponent<Rigidbody>().AddForce(head.forward * 5000);
+        
+        //10 is amount of seconds after which projectile are destroyed
+        Destroy(clone, 10);
+    }
+}
