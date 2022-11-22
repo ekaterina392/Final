698f41e26734c0abb31fab1147d62ac2cf89d9cd
Ekaterina Abramovich <112434872+ekaterina392@users.noreply.github.com>
2022-11-22 12:12:46 -0500

    Running animation fixed

--- Final project/Assets/Scripts (scene 3)/Guard.cs
+++ Final project/Assets/Scripts (scene 3)/Guard.cs
@@ -6,87 +6,62 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
 
-namespace Ekaterina {
-    public class Guard : MonoBehaviour
-    {
-        private Animator Zorlik;
-
-        NavMeshAgent _agent;
-        public Transform[] targetLocations;
-        public Transform playerTarget;
-        private int _targetIndex = 0;
-        public float sight = 10f;
+public class Guard : MonoBehaviour
+{
+    private Animator Zorlik;
+    public static float speed = 5f;
 
 
-        public Transform lastActiveWayPoint; //to keep track of where we were going so we know to go back to it
-        public Transform activeTargetLocation;// for debugging
+    NavMeshAgent _agent;
+    public Transform[] targetLocations;
+    public Transform playerTarget;
+    private int _targetIndex = 0;
+    public float sight = 10f;
         
-        private bool _isChasing = false;
-        int layerMask = 1 << 3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Zorlik = gameObject.GetComponent<Animator>();
 
-        // Start is called before the first frame update
-        void Start()
-        {
-            _agent = transform.GetComponent<NavMeshAgent>();
-            activeTargetLocation = lastActiveWayPoint = targetLocations[0]; //set the default targets
-            Zorlik = gameObject.GetComponent<Animator>();
+        _agent = transform.GetComponent<NavMeshAgent>();
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        Zorlik.SetBool("isChasing", false);
+
+        _agent.speed = speed;
+
+        int layerMask = 1 << 3;
+        if (Vector3.Distance(transform.position,playerTarget.position)<sight 
+            && !Physics.Linecast (transform.position, playerTarget.transform.position,layerMask)) {
+            _agent.SetDestination(playerTarget.position);
+            
+            //Running animation set up
+            Zorlik.SetBool("isChasing", true);
+            
+            //Increase enemy speed after noticing the player
+            _agent.speed = 50f;
 
         }
-
-        // Update is called once per frame
-        void Update()
+        if (!_agent.pathPending)
         {
-            //if the player is suddenly shaded
-            if (playerTarget.GetComponent<MarsPlayer>().IsShaded &&
-                activeTargetLocation != lastActiveWayPoint)
+            if (_agent.remainingDistance <= _agent.stoppingDistance)
             {
-                Zorlik.SetBool("isChasing", false);
-                _agent.SetDestination(lastActiveWayPoint.position);
-                activeTargetLocation = lastActiveWayPoint;
-            }
-            //if the player is still visible
-            if (IsPlayerVisible())
-            {
-                lastActiveWayPoint = targetLocations[_targetIndex];
-                _agent.SetDestination(playerTarget.position);
-                activeTargetLocation = playerTarget;
-                
-                //Running animation set up
-                Zorlik.SetBool("isChasing", true);
-            }
-            else //if we're normally moving, this remains unchanged
-            {
-                if (!_agent.pathPending)
+
+                if (!_agent.hasPath || _agent.velocity.sqrMagnitude == 0f)
                 {
-                    if (_agent.remainingDistance <= _agent.stoppingDistance)
-                    {
-                        if (!_agent.hasPath || _agent.velocity.sqrMagnitude == 0f)
-                        {
-                            GoToNextLocation();
-                        }
-                    }
+                    GoToNextLocation();
                 }
             }
-            
         }
-
-
-        // Separate function because we will do this evaluation mutiple times
-        bool IsPlayerVisible()
-        {
-            return Vector3.Distance(transform.position, playerTarget.position) < sight
-                && !Physics.Linecast(transform.position, playerTarget.transform.position, layerMask) 
-                && !playerTarget.GetComponent<MarsPlayer>().IsShaded;
-            
-        }
-
-        public void GoToNextLocation()
-        {
-            _agent.SetDestination(targetLocations[_targetIndex].position);
-            _targetIndex = (_targetIndex+ 1) % targetLocations.Length;
-            
-            activeTargetLocation = targetLocations[_targetIndex];
-        }
-        
     }
+
+    public void GoToNextLocation()
+    {
+        _agent.SetDestination(targetLocations[_targetIndex].position);
+        _targetIndex = (_targetIndex+ 1) % targetLocations.Length;
+    }
+        
 }
