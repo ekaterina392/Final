f57d5c71c9de4f92fd6761145df73c99551f4780
Ekaterina Abramovich <112434872+ekaterina392@users.noreply.github.com>
2022-11-27 22:16:06 -0500

    Health added to the player and enemies

--- Final project/Assets/Scripts (scene 3)/EnemyAttacked.cs
+++ Final project/Assets/Scripts (scene 3)/EnemyAttacked.cs
@@ -1,33 +1,67 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyAttacked : MonoBehaviour
 {
-    public ParticleSystem enemyFire1;
-    public ParticleSystem enemyFire2;
-    public ParticleSystem enemyFire3;
-    public ParticleSystem enemyFire4;
-    public ParticleSystem enemyFire5;
-
-
+    public int Health = 100;
+    int Damage = 1;
     
+    //Here should go sounds
+
+    public bool InAttackRange = false;
+    
+    public ParticleSystem enemyFire1;
+    //public ParticleSystem enemyFire2;
+    //public ParticleSystem enemyFire3;
+    //public ParticleSystem enemyFire4;
+    //public ParticleSystem enemyFire5;
+
+    //If enemy's health is zero
+    
+    /*
+    private void Update()
+    {
+        if (Health <= 0)
+        {
+            Time.timeScale = 0;
+        }
+    } */
+
+    private void Update()
+    {
+        InAttackRange = false;
+    }
+
     private void OnParticleCollision(GameObject collision)
     {
         if (collision.CompareTag("Enemy"))
         {
-            Debug.Log("collision");
+            InAttackRange = true;
+            StartCoroutine(HurtEnemy());
+            
+            //Debug.Log("collision");
             CreateParticles();
+        }
+    }
+    
+    IEnumerator HurtEnemy()
+    {
+        while (true && InAttackRange == true)
+        {
+            Health -= Damage;
+            yield return new WaitForSeconds(3f);
         }
     }
 
     void CreateParticles()
     {
         enemyFire1.Play();
-        enemyFire2.Play();
-        enemyFire3.Play();
-        enemyFire4.Play();
-        enemyFire5.Play();
+        //enemyFire2.Play();
+        //enemyFire3.Play();
+        //enemyFire4.Play();
+        //enemyFire5.Play();
     }
 
 }
---
+++ Final project/Assets/Scripts (scene 3)/PlayerHealth.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public GameObject Player;
+    public int Health = 100;
+    int Damage = 10;
+    public GameObject HealthDisplay;
+    
+    //Here should go sounds
+
+    public GameObject EndMenu;
+    public bool InAttackRange = false;
+
+    private void Awake()
+    {
+        EndMenu.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.transform.tag == "Enemy")
+        {
+            InAttackRange = true;
+            StartCoroutine(HurtPlayer());
+        }
+    }
+
+    private void OnTriggerExit(Collider collision)
+    {
+        InAttackRange = false;
+    }
+
+    IEnumerator HurtPlayer()
+    {
+        while (true && InAttackRange == true)
+        {
+            Health -= Damage;
+            HealthDisplay.GetComponent<TMP_Text>().text = "" + Health;
+            yield return new WaitForSeconds(3f);
+        }
+    }
+
+    private void Update()
+    {
+        if (Health <= 0)
+        {
+            HealthDisplay.GetComponent<TMP_Text>().text = "" + "0";
+            Time.timeScale = 0;
+            EndMenu.SetActive(true);
+            
+            //disable player controller script
+            Player.GetComponent<ThirdPersonMovement>().enabled = false;
+            
+            Cursor.visible = true;
+            Screen.lockCursor = false;
+        }
+    }
+}
