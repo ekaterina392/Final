2a0dbd791a06d03307978ba83c306f7344a42cfe
Ekaterina Abramovich <112434872+ekaterina392@users.noreply.github.com>
2022-12-03 16:55:37 -0500

    Player added to Level 4

---
+++ Final project/Assets/Scene 4/Scripts/ThirdPersonMovementScene4.cs
@@ -0,0 +1,119 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class ThirdPersonMovementScene4 : MonoBehaviour
+{
+    //Default to 3 second cooldown
+    public float Cooldown = 3f;
+ 
+    //Used as a count down timer
+    public float CooldownCountdown = 0f;
+    
+    public ParticleSystem fireBreath;
+
+    private Animator DragonAnimator;
+
+    public CharacterController controller;
+
+    public Transform camera;
+
+    private float speed = 4;
+
+    public float turnSmoothTime = 0.1f;
+    private float turnSmoothVelocity;
+    
+    //Jumping
+    
+    public float gravity = -9.81f;
+    public float jumpHeight = 3f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    private Vector3 velocity;
+
+    private bool isGrounded;
+    private KeyCode jumpKey = KeyCode.RightShift;
+    
+    /*
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.name == "cube")
+        {
+            Debug.Log("collision");
+        }
+    } */
+    
+    void Start()
+    {
+        DragonAnimator = gameObject.GetComponent<Animator>();
+    }
+    
+    void Update()
+    {
+        //float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(0f, 0f, vertical).normalized;
+
+        //Walking
+        if (direction.magnitude >= 0.1f)
+        {
+            float tragetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, tragetAngle, ref turnSmoothVelocity,
+                turnSmoothTime);
+            
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDirection = Quaternion.Euler(0f, tragetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDirection.normalized * speed * Time.deltaTime);
+            
+            DragonAnimator.ResetTrigger("fly");
+            DragonAnimator.ResetTrigger("idle");
+
+            DragonAnimator.SetTrigger("walk");
+        }
+
+        //Idle
+        if (direction.magnitude < 0.1f)
+        {
+            DragonAnimator.ResetTrigger("walk");
+            DragonAnimator.ResetTrigger("fly");
+            DragonAnimator.ResetTrigger("breathFire");
+
+            DragonAnimator.SetTrigger("idle");
+        }
+
+        //Jumping
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+        
+        
+        //Stops Fly Fire Breath if incorrect animation is played
+        if (DragonAnimator.GetCurrentAnimatorStateInfo(0).IsName("idle"))
+        {
+            StopParticles();
+        }
+        
+        if (DragonAnimator.GetCurrentAnimatorStateInfo(0).IsName("walk"))
+        {
+            StopParticles();
+        }
+
+    }
+    
+    void CreateParticles()
+    {
+        fireBreath.Play();
+    }
+    
+    void StopParticles()
+    {
+        fireBreath.Stop();
+    }
+    
+}
