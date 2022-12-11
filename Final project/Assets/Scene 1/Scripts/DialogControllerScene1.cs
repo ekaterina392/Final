fe43129d9a25b359c258fc104e5e9f2f0aae8d93
Ekaterina Abramovich <112434872+ekaterina392@users.noreply.github.com>
2022-12-11 18:57:19 -0500

    Transition to the next scene added

--- Final project/Assets/Scene 1/Scripts/DialogControllerScene1.cs
+++ Final project/Assets/Scene 1/Scripts/DialogControllerScene1.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class DialogControllerScene1 : MonoBehaviour
 {
@@ -40,23 +41,30 @@ public class DialogControllerScene1 : MonoBehaviour
 
     void Update()
     {
+        
+               
+        if (Input.GetKeyDown(KeyCode.Space) && Scream == null)
+        {
+            SceneManager.LoadScene("Scene 2");
+        }
+        
 
-        if (Vector3. Distance(gameObject.transform.position, Girl.transform.position) < 2 && Input.GetKeyDown(KeyCode.T))
+        if (Vector3.Distance(gameObject.transform.position, Girl.transform.position) < 2 && Input.GetKeyDown(KeyCode.T))
         {
             CanvasGirl.SetActive(true);
             Destroy(CanvasPressT);
         }
-        
+
         if (Input.GetKeyDown(KeyCode.Q))
         {
             CanvasPressE.SetActive(true);
             Destroy(CanvasGirl);
             Knocking.Play();
-                
+
             GirlSpeaking.PlayDelayed(2.5f);
             DoorTrigger.SetActive(true);
         }
-        
+
         if (Vector3.Distance(gameObject.transform.position, DoorTrigger.transform.position) < 2.5f &&
             Input.GetKeyDown(KeyCode.E) && DoorTrigger.activeSelf)
         {
@@ -65,9 +73,9 @@ public class DialogControllerScene1 : MonoBehaviour
             Destroy(DoorTrigger);
             Knocking.Stop();
             GirlSpeaking.Stop();
-            
+
             DoorSound.Play();
-            
+
             //Timer
             IEnumerator ExecuteAfterTime1(float time)
             {
@@ -75,26 +83,30 @@ public class DialogControllerScene1 : MonoBehaviour
 
                 Scream.Play();
             }
+
             StartCoroutine(ExecuteAfterTime1(3));
-            
-            
+
+
             //Timer
             IEnumerator ExecuteAfterTime2(float time)
             {
                 yield return new WaitForSeconds(time);
-                
+
                 Text.SetActive(true);
             }
+
             StartCoroutine(ExecuteAfterTime2(10));
-            
+
             IEnumerator ExecuteAfterTime3(float time)
             {
                 yield return new WaitForSeconds(time);
-                
+
+                Destroy(Scream);
                 Button.SetActive(true);
             }
+
             StartCoroutine(ExecuteAfterTime3(17));
         }
     }
-
+    
 }
