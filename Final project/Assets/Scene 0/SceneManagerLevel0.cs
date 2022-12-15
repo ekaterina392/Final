7919ad19cf89d053ee1af0184feea04064870a18
Ekaterina Abramovich <112434872+ekaterina392@users.noreply.github.com>
2022-12-14 21:48:00 -0500

    Start scene added

---
+++ Final project/Assets/Scene 0/SceneManagerLevel0.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagerLevel0 : MonoBehaviour
+{
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene("Scene 1");
+        }
+
+    }
+}
